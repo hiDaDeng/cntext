efe08c32072c6c61a448ffd70ca39ea327a2824e
DaDeng <thunderhit@qq.com>
2022-11-14 15:48:22 +0800

    add dat score

---
+++ build/lib/cntext/__init__.py
@@ -0,0 +1,11 @@
+__version__ = "1.8.1"
+
+from cntext.dictionary import SoPmi, W2VModels, co_occurrence_matrix, Glove
+from cntext.similarity import jaccard_sim, minedit_sim, simple_sim, cosine_sim
+from cntext.stats import load_pkl_dict, dict_pkl_list, term_freq, readability, sentiment, sentiment_by_valence, sentiment_by_weight
+from cntext.mind import Text2Mind
+
+
+
+
+
